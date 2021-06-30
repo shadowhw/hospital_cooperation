41d871aaf9b31129b901ab2bbf526f6161ca186c
shadowhw <shadowhw@yeah.net>
2021-06-30 17:06:56 +0800

    第一次提交项目

---
+++ hl-admin/src/test/java/com/hl/ApplicationTest.java
@@ -0,0 +1,23 @@
+package com.hl;
+
+import org.junit.Test;
+public class ApplicationTest
+{
+
+
+    @Test
+    void contextLoads()
+    {
+
+    }
+
+    //代码生成器
+    @Test
+    void codeGenerator(){
+        //CodeGenerator.codeGenerator("fcsd","fcsd");
+    }
+
+
+
+
+}
