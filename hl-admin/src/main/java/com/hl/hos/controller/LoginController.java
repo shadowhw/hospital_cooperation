ffaba1111e152a9c92f0bd99dfb07a1a7c5b7e98
jianyi <1261794911@qq.com>
2021-07-06 13:52:09 +0800

    完善根据单位名称登录

--- hl-admin/src/main/java/com/hl/hos/controller/LoginController.java
+++ hl-admin/src/main/java/com/hl/hos/controller/LoginController.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
 import com.hl.hos.mapper.Doctor_infoMapper;
 import com.hl.hos.pojo.Doctor_info;
+import com.hl.hos.pojo.Hos_info;
 import com.hl.hos.service.Doctor_infoService;
+import com.hl.hos.service.Hos_infoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,30 +20,42 @@ public class LoginController {
     @Autowired
     private Doctor_infoService doctorService;
 
+    @Autowired
+    private Hos_infoService hosService;
+
 
     @PostMapping("/userLogin")
     public String userLogin(@RequestParam("doctorName")String hos_name ,
                             @RequestParam("doctorPwd")String doctorPwd ,
                             HttpSession session ){
 
-        Doctor_info doctorInfo = new Doctor_info();
-        doctorInfo.setDoctor_pwd(doctorPwd);
-        //
-        Doctor_info loginUser = doctorService.getOne(new QueryWrapper<Doctor_info>().gt("doctor_pwd",doctorInfo.getDoctor_pwd()));
-
-        if(loginUser == null){
+        //查询有误单位
+        Hos_info hosInfo = hosService.getOne(new QueryWrapper<Hos_info>().eq("hos_name",hos_name));
+        if(hosInfo == null){
+            session.setAttribute("error","账号或者密码错误");
+            return "redirect:/login";
+        }
+        //根据hosId查询医生
+        Doctor_info doctorInfo = doctorService.getOne(new QueryWrapper<Doctor_info>().eq("hos_id", hosInfo.getId()));
+        if(doctorInfo == null){
+            session.setAttribute("error","账号或者密码错误");
+            return "redirect:/login";
+        }
+        //验证密码
+        if(doctorInfo.getDoctor_pwd() != doctorPwd){
             session.setAttribute("error","账号或者密码错误");
             return "redirect:/login";
         }
 
-        Integer pass = loginUser.getPass();
+
+        Integer pass = doctorInfo.getPass();
         if(pass==0){ //账号没激活
             session.setAttribute("error","账号没有激活");
             return "redirect:/login";
         }
 
         String addr = "/login";
-        Integer stat = loginUser.getStat();//获取状态码
+        Integer stat = doctorInfo.getStat();//获取状态码
         session.setAttribute("doctor_info",doctorInfo);
         if(stat == 0){ //管理员
             addr = "adminIndex";
