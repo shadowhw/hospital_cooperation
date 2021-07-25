7f348a1c3694f8e0dfe0135fd9a7816aa6374472
shadowhw <shadowhw@yeah.net>
2021-07-25 15:02:11 +0800

    报表导出

--- hl-admin/src/main/java/com/hl/hos/controller/Hos_infoController.java
+++ hl-admin/src/main/java/com/hl/hos/controller/Hos_infoController.java
@@ -1,9 +1,20 @@
 package com.hl.hos.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hl.hos.pojo.Hos_info;
+import com.hl.hos.pojo.Result;
+import com.hl.hos.service.Hos_infoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +27,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hos/hos_info")
 public class Hos_infoController {
+    @Autowired
+    private Hos_infoService hos_infoService;
+    @Autowired
+    private Result result;
 
+    /**
+     * 查询所有医院信息
+     * @return
+     */
+    @ResponseBody
+    @GetMapping("/get_hos_list")
+    public Result get_hos_list(String page, String limit)
+    {
+        List<Hos_info> list = hos_infoService.list();
+
+        Page<Hos_info> page1 = new Page<Hos_info>(Integer.parseInt(page),Integer.parseInt(limit));
+        IPage<Hos_info> iPage = hos_infoService.page(page1);
+
+        result.setCount(list.size());//数量应该是所有数据的大小
+        result.setData(iPage.getRecords());
+        result.setCode(200);
+        return result;
+    }
 }
 
